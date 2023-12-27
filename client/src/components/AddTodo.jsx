64670bb3818c3906317bde06b63de04703e88b45
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-27 11:51:44 +0545

    Style :  addtodo component with state management

--- client/src/components/AddTodo.jsx
+++ client/src/components/AddTodo.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTodo = () => {
+  const [input, setInput] = useState();
+  console.log(input);
   const submitHandler = (e) => {
     e.preventDefault();
   };
   return (
-    <div>
-      <form onSubmit={submitHandler}>
-        <input type="text" placeholder="write your todo here" />
-        <button>Add Todo</button>
+    <div className="max-w-xs mx-auto mt-6">
+      <form onSubmit={submitHandler} className="flex items-center">
+        <input
+          type="text"
+          className="border border-gray-300 p-2 w-2/3"
+          placeholder="Add Todo"
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        <button
+          type="submit"
+          className="ml-2 bg-blue-500 text-white p-2 rounded"
+        >
+          Add Todo
+        </button>
       </form>
     </div>
   );
