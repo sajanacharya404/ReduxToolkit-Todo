011c1a6c50f54cea505ba012793a0cf855d8bc36
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-27 12:59:23 +0545

    removeTodo funcunality completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AddTodo from "./components/AddTodo";
+import Todo from "./components/Todo";
 
 const App = () => {
   return (
     <div>
       <AddTodo />
+      <Todo />
     </div>
   );
 };
