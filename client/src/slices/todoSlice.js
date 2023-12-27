ed602431e2c7ca204cda9ac2a5b0388da9dc52a3
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-27 11:45:13 +0545

    updateTodo action created

--- client/src/slices/todoSlice.js
+++ client/src/slices/todoSlice.js
@@ -16,8 +16,15 @@ const todoSlice = createSlice({
   removeTodo: (state, action) => {
     state.todos = state.todos.filter((todo) => todo.id !== action.payload);
   },
+  updateTodo: (state, action) => {
+    const { id, updatedTodo } = action.payload;
+    const todo = state.todos.find((todo) => todo.id === id);
+    if (todo) {
+      Object.assign(todo, updatedTodo);
+    }
+  },
 });
 
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
